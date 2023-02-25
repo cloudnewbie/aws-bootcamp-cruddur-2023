624af56fd1c56bc26570831059ea65c99e36ab11
Gary B <gary.bhandarkar@gmail.com>
2023-02-25 23:29:11 +0000

    React aded import

--- frontend-react-js/src/components/ActivityActionLike.js
+++ frontend-react-js/src/components/ActivityActionLike.js
@@ -1,4 +1,4 @@
-import React, { Component }  from 'react';
+import React from 'react';
 import {ReactComponent as HeartIcon} from './svg/heart.svg';
 
 export default function ActivityActionLike(props) { 
