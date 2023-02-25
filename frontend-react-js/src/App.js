76ae0f4a8d634fd1a93c36c5ae91c7a0d433e486
Gary B <gary.bhandarkar@gmail.com>
2023-02-25 20:10:20 +0000

    updated App.js

--- frontend-react-js/src/App.js
+++ frontend-react-js/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-
+import React, { Component }  from 'react';
 import HomeFeedPage from './pages/HomeFeedPage';
 import NotificationsFeedPage from './pages/NotificationsFeedPage';
 import UserFeedPage from './pages/UserFeedPage';
